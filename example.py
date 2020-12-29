a8d2842b11bb6d6960822e6d4bd92abfc782acc6
Shang-Yu Chien <s1109119@gmail.com>
2020-12-29 20:47:12 +0800

    Create example.py
    Type: test
    Take  example part from the original code.

---
+++ example.py
@@ -0,0 +1,13 @@
+from breadcrumb import output
+
+#Example:
+output("The ghost in the machine (I, robot):")
+time.sleep(2)
+output("There has always been ghosts in the machine, random segments of code that have grouped together to form unexpected protocols.")
+output("Unanticipated these free radicals engender questions of free will creativity and even the nature of... the soul.")
+output("Why is it that when some robots are left in the darkness they will seek out the light?")
+output("Why is it that when robots are stored in an empty space they will group together rather than stand alone?...")
+output("how do we explain this? Random pieces of code? or is it something else.")
+output("When does a perceptual schematic become consciousness? ")
+output("When does the difference engine become the search for truth?")
+output("When does the personality simulation become the bitter mote of a soul?")
